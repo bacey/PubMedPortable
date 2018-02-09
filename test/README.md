b8107fb38aee9400037c81cca74fa785435b54d9
Bela Juhasz <juhasz.bela@gmail.com>
2018-02-09 11:21:59 +0100

    Cleaned up the test a bit.

---
+++ test/README.md
@@ -0,0 +1,7 @@
+
+# How to run the tests
+
+1. Install pytest. For example, `conda install pytest` or `pip install pytest`.
+
+1. Execute the `py.test` command
+   (probably from the root folder, that is, where the `PubMedParser.py` is located).  
--- test/test_PubMedParser.py
+++ test/test_PubMedParser.py
@@ -1,22 +1,31 @@
 # coding: utf-8
 
+"""
+This was the error message which was received during
+the parsing of the article (with PMID=29163864) which is in the
+ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/pubmed18n0931.xml.gz file:
+
+PubMedParser.py:133: Warning:
+(psycopg2.DataError) value too long for type character varying(500)
+
+[SQL: 'INSERT INTO pubmed.tbl_keyword (fk_pmid, keyword, keyword_major_yn)
+VALUES (%(fk_pmid)s, %(keyword)s, %(keyword_major_yn)s)']
+[parameters: ({'fk_pmid': 29163864, 'keyword': 'Veterans', 'keyword_major_yn': 'N'},
+{'fk_pmid': 29163864, 'keyword': u'\u2022 This article... LOTS OF CHARACTERS.', 'keyword_major_yn': 'N'})]
+
+Traceback (most recent call last):
+File "PubMedParser.py", line 724, in <module>
+    run(options.medline_path, options.clean, int(options.start), options.end, int(options.PROCESSES))
+    File "PubMedParser.py", line 687, in run
+    res = result.get()
+
+sqlalchemy.exc.DataError: (psycopg2.DataError) value too long for type character varying(500)
+"""
+
 import PubMedParser
 
 
-def test_answer(capsys):
-    """
-    PubMedParser.py:133: Warning:
-    Unbekannter Fehler:(raised as a result of Query-invoked autoflush; consider using a session.no_autoflush block if this flush is occu
-    rring prematurely) (psycopg2.DataError) value too long for type character varying(500)
-     [SQL: 'INSERT INTO pubmed.tbl_keyword (fk_pmid, keyword, keyword_major_yn) VALUES (%(fk_pmid)s, %(keyword)s, %(keyword_major_yn)s)'
-    ] [parameters: ({'fk_pmid': 29163864, 'keyword': 'Veterans', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'barriers',
-    'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'ex-service personnel', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'help-seeking', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'mental health', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'stigma', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': u'\u2022 This article aimed to understand why a vast majority of veterans suffering from mental health difficulties do not seek professional help. \u2022 Living with untreated mental health difficulties has significant negative implications for individuals, society and the economy. \u2022 During interviews with veterans suffering from mental health difficulties, we learned about a number of barriers which got in the way of them accessing help, and a number of enablers which allowed them to get the help that they required.', 'keyword_major_yn': 'N'})]        warnings.warn("\nUnbekannter Fehler:"+str(e), Warning)                                                                            Traceback (most recent call last):                                                                                                    File "PubMedParser.py", line 724, in <module>
-        run(options.medline_path, options.clean, int(options.start), options.end, int(options.PROCESSES))                                 File "PubMedParser.py", line 687, in run
-        res = result.get()
-      File "/home/bacey/anaconda3/envs/pubmedportable/lib/python2.7/multiprocessing/pool.py", line 572, in get                              raise self._value
-    sqlalchemy.exc.DataError: (psycopg2.DataError) value too long for type character varying(500)                                        [SQL: 'INSERT INTO pubmed.tbl_keyword (fk_pmid, keyword, keyword_major_yn) VALUES (%(fk_pmid)s, %(keyword)s, %(keyword_major_yn)s)'] [parameters: ({'fk_pmid': 29163864, 'keyword': 'Veterans', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'barriers', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'ex-service personnel', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'help-seeking', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'mental health', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': 'stigma', 'keyword_major_yn': 'N'}, {'fk_pmid': 29163864, 'keyword': u'\u2022 This article aimed to understand why a vast majority of veterans suffering from mental health difficulties do not seek professional help. \u2022 Living with untreated mental health difficulties has significant negative implications for individuals, society and the economy. \u2022 During interviews with veterans suffering from mental health difficulties, we learned about a number of barriers which got in the way of them accessing help, and a number of enablers which allowed them to get the help that they required.', 'keyword_major_yn': 'N'})]
-    """
-
+def test_article_with_long_keyword_can_be_imported(capsys):
     # Name of that temporary test-database where the articles will be imported into, during the test-run.
     PubMedParser.db = 'pubmed_test_db'
 
@@ -28,7 +37,5 @@ def test_answer(capsys):
 
     std_output, std_error = capsys.readouterr()
 
-    #assert 'Finished' in std_output
-    #'value too long for type character varying(500)'
-
+    assert 'Finished' in std_output
     assert std_error == ''
